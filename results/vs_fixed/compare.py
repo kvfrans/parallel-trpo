55b5d17115d74cb65f3e5ab18931453b45d173d5
Kevin Frans <kevin@bobthechicken.com>
2016-11-23 23:57:22 -0800

    res

---
+++ results/vs_fixed/compare.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import sys
+
+times = []
+rewards = []
+t = []
+r = []
+
+trials = ["Reacher-v1-adaptive-1000.000000-0.001000-300.000000-0.000000","Reacher-v1-adaptive-1000.000000-0.001000-300.000000-0.000500","Reacher-v1-adaptive-20000.000000-0.001000-0.000000-0.000500","Reacher-v1-none-1500.000000-0.001000-0.000000-0.000000","Reacher-v1-none-20000.000000-0.010000-0.000000-0.000000"]
+names = ["Adapt steps", "Adapt steps and KL", "Adapt KL", "fixed steps", "fixed KL"]
+for i in xrange(len(trials)):
+    with open(trials[i]) as data_file:
+        data = json.load(data_file)
+
+    times.append([])
+    rewards.append([])
+    totaltime = 0
+
+    time_since = 0
+    avg = 0
+    avgcount = 0
+
+    for e in xrange(len(data["mean_reward"])):
+        totaltime += data["timesteps"][e]
+
+        time_since += data["timesteps"][e]
+        avg += data["mean_reward"][e]
+        avgcount += 1
+
+        if time_since > 10000:
+            time_since = 0
+            # totaltime += 1
+            if i == 0:
+                times[i].append(totaltime)
+            else:
+                times[i].append(totaltime)
+            rewards[i].append(avg/avgcount)
+
+            avg = 0
+            avgcount = 0
+
+    t.append(np.array(times[i]))
+    r.append(np.array(rewards[i]))
+
+    plt.plot(t[i],r[i],color=(1 - (i/5.0),i/5.0,1.0),label=names[i])
+
+plt.xlabel("Environment Steps Seen")
+plt.ylabel("Average return")
+plt.legend(loc=4)
+plt.title("Reacher-v1")
+plt.show()
---
+++ results/vs_fixed/fixed_kl.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import sys
+
+times = []
+rewards = []
+t = []
+r = []
+
+trials = ["Reacher-v1-none-20000.000000-0.001000-0.000000-0.000000","Reacher-v1-none-20000.000000-0.005000-0.000000-0.000000","Reacher-v1-none-20000.000000-0.010000-0.000000-0.000000"]
+names = ["Fixed 0.001 KL", "Fixed 0.005 KL", "Fixed 0.01 KL"]
+
+for i in xrange(len(trials)):
+    with open(trials[i]) as data_file:
+        data = json.load(data_file)
+
+    times.append([])
+    rewards.append([])
+    totaltime = 0
+
+    time_since = 0
+    avg = 0
+    avgcount = 0
+
+    for e in xrange(len(data["mean_reward"])):
+        totaltime += data["timesteps"][e]
+
+        time_since += data["timesteps"][e]
+        avg += data["mean_reward"][e]
+        avgcount += 1
+
+        if time_since > 10000:
+            time_since = 0
+            # totaltime += 1
+            if i == 0:
+                times[i].append(totaltime)
+            else:
+                times[i].append(totaltime)
+            rewards[i].append(data["mean_reward"][e])
+
+            avg = 0
+            avgcount = 0
+
+    t.append(np.array(times[i]))
+    r.append(np.array(rewards[i]))
+
+    plt.plot(t[i],r[i],color=(1 - (i/5.0),i/5.0,1.0),label=names[i])
+
+plt.xlabel("Environment Steps Seen")
+plt.ylabel("Average return")
+plt.legend(loc=4)
+plt.title("Reacher-v1")
+plt.show()
